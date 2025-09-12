c982103364d891413a8de33617483b32a23ef994
muzhiwa <muzhdawafa2008@gmail.com>
2025-09-12 19:12:08 +0430

    updated app.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Translation from "./components/Translation";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Translation />
+    </div>
+  );
+}
+
+export default App;
